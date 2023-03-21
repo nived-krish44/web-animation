33b8513312e7b2c6c044baec5401c4eb72c41e8e
nived-krish44 <114802043+nived-krish44@users.noreply.github.com>
2023-03-21 17:59:29 +0530

    Add files via upload

---
+++ scope.js
@@ -0,0 +1,7 @@
+const div = document.createElement("div");
+div.style.width = "100px";
+div.style.height = "100px";
+div.style.background = "red";
+div.style.color = "white";
+div.innerHTML = "Hello";
+document.body.appendChild(div);
